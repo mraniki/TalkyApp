294285f56dca9b97811e63eb7f1478035de83f76
mraniki <mraniki@users.noreply.github.com>
2024-01-08 22:50:11 +0100

    Update app.js and sw.js files

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 console.log('I am running!');
-document.write('hello 004');
+document.write('hello 005');
 
 document.addEventListener('DOMContentLoaded', function () {
-  const audioPlayer = document.querySelector('audio');
-  audioPlayer.src = 'https://somafm.com/m3u/live130.m3u';
+  const audioPlayer = new Audio('https://somafm.com/m3u/live130.m3u');
+  audioPlayer.controls = true;
+  document.body.appendChild(audioPlayer);
+  audioPlayer.play();
 });
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/talkyapp';
 var APP_PREFIX = 'ttpwa_';
-var VERSION = 'version_004';
+var VERSION = 'version_005';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
