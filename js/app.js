14a39f7f68c670913091dbffd65b83d3ef8acd19
mraniki <mraniki@users.noreply.github.com>
2024-01-08 23:11:30 +0100

    Update app.js file with new changes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,24 @@
 console.log('I am running!');
-document.write('hello 006');
+document.write('hello 007');
 
 
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const audioPlayer = new Audio('http://radio.plaza.one/mp3');
+//   audioPlayer.controls = true;
+//   document.body.appendChild(audioPlayer);
+//   audioPlayer.play();
+// });
 
 document.addEventListener('DOMContentLoaded', function () {
+  const iframe = document.createElement('iframe');
+  iframe.width = '800';
+  iframe.height = '600';
+  iframe.frameBorder = '0';
+  
   const audioPlayer = new Audio('https://somafm.com/m3u/live130.m3u');
   audioPlayer.controls = true;
-  document.body.appendChild(audioPlayer);
-  audioPlayer.play();
+
+  iframe.contentDocument.body.appendChild(audioPlayer);
+  document.body.appendChild(iframe);
 });
