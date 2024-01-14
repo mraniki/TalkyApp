05976691b943b1f5519c88f4acf581fb19435e1b
mraniki <mraniki@users.noreply.github.com>
2024-01-14 22:03:48 +0100

    Update audioPlayer URL in app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 console.log('I am running!');
 
-document.write('hello 013');
+document.write('hello 014');
 
 
 
 document.addEventListener('DOMContentLoaded', function () {
-  const audioPlayer = new Audio('https://somafm.com/m3u/live.m3u');
+  const audioPlayer = new Audio('http://s-00.wefunkradio.com:81/wefunk64.mp3');
   audioPlayer.controls = true;
   document.body.appendChild(audioPlayer);
   audioPlayer.play();
